d09ccb7d62a1bf1db5bcc38bffe96d86b1e112f7
wrewsama <andrewlzs512@gmail.com>
2023-06-11 18:00:58 +0800

    set up selenium script

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,1 +1,15 @@
-print("test")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Set Chrome options to run in headless mode
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://schedule.hololive.tv/")
+
+driver.implicitly_wait(10)
+driver.save_screenshot("screenshot.png")
+driver.quit()
